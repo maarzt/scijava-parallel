71b078b7058a42bb6861dd14a0f6c03e13126002
Jan Kožusznik <jan@kozusznik.cz>
2018-03-14 11:39:22 +0100

    refactor: add posibility to call remote command

--- src/main/java/cz/it4i/parallel/SimpleOstravaParadigm.java
+++ src/main/java/cz/it4i/parallel/SimpleOstravaParadigm.java
@@ -22,21 +22,27 @@ import org.slf4j.LoggerFactory;
 @Plugin(type = ParallelizationParadigm.class)
 public class SimpleOstravaParadigm extends AbstractParallelizationParadigm {
 
+	private static final String PORT = "port";
+
+	private static final String ADDRESS = "address";
+
 	public static final Logger log = LoggerFactory.getLogger(cz.it4i.parallel.SimpleOstravaParadigm.class);
-	
+
 	private WorkerPool workerPool;
-	
+
 	@Override
 	public void init() {
-		
+
 		retrieveConnectionConfig();
 		if (connectionConfig.size() == 0) {
 			Map<String, String> configEntries = new LinkedHashMap<>();
-			configEntries.put("address", "localhost");
-			configEntries.put("port", "8080");
+			configEntries.put(ADDRESS, "localhost");
+			configEntries.put(PORT, "8080");
 			updateConnectionConfig(configEntries);
 		}
 		workerPool = new WorkerPool();
+		workerPool.addWorker(new ImageJServerWorker(connectionConfig.get(ADDRESS),
+				Integer.parseInt(connectionConfig.get(PORT))));
 	}
 
 	@Override
@@ -48,114 +54,118 @@ public class SimpleOstravaParadigm extends AbstractParallelizationParadigm {
 
 	@Override
 	public <T> void parallelLoop(Iterable<T> arguments, BiConsumer<T, ParallelTask> consumer) {
-		arguments.forEach(val->
-		{
-			try(P_ParallelTask task = new P_ParallelTask()){
-				consumer.accept(val,null);
+		arguments.forEach(val -> {
+			try (P_ParallelTask task = new P_ParallelTask()) {
+				consumer.accept(val, null);
 			}
 		});
 
 	}
-	
-	
+
 	private class P_ParallelTask implements ParallelTask, Closeable {
 
-		private ParallelWorker p;
-		
+		private ParallelWorker worker;
+
+		private String executeResult;
+
 		public P_ParallelTask() {
 			try {
-				p = workerPool.takeFreeWorker();
+				worker = workerPool.takeFreeWorker();
 			} catch (InterruptedException e) {
 				log.error(e.getMessage(), e);
 			}
 		}
-		
+
 		@SuppressWarnings("unchecked")
 		@Override
 		public <T> T getRemoteModule(Class<T> type) {
-			return (T) Proxy.newProxyInstance(this.getClass().getClassLoader(), new Class<?>[] {type}, new P_InvocationHandler(type, p));
+			return (T) Proxy.newProxyInstance(this.getClass().getClassLoader(), new Class<?>[] { type },
+					new P_InvocationHandler(type));
 		}
 
 		@Override
 		public void close() {
-			workerPool.addWorker(p);
-		}
-		
-	}
-
-	private class P_InvocationHandler implements InvocationHandler {
-
-		private Map<String,Object> args = new HashMap<>();
-		private String typeName;
-		private ParallelWorker worker;
-		private String executeResult;
-		
-		
-		public P_InvocationHandler(Class<?> type, ParallelWorker worker) {
-			this.typeName = getTypeName(type);
-			this.worker = worker;
-		}
-		
-		//TODO make better connection to real type - e.g. based on annotations 
-		private String getTypeName(Class<?> type) {
-			return type.getPackage().getName() + type.getSimpleName().substring(1);
+			workerPool.addWorker(worker);
 		}
 
 		@Override
-		public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
-			if(method.getName().startsWith("set")) {
-				setValue(getPropertyName(method.getName()), args[0]);
-			} else if(method.getName().equals("run")) {
-				// execute worker
-				executeResult = worker.executeModule( "command:"+typeName,  this.args);
+		public <T> void run(Class<T> type, Map<String, Object> inputMap) {
+			executeResult = worker.executeModule("command:" + type, inputMap);
 
-			} else if(method.getName().startsWith("get")) {
-				return getValue(getPropertyName(method.getName()),method.getReturnType());
+		}
+
+		private class P_InvocationHandler implements InvocationHandler {
+
+			private Map<String, Object> args = new HashMap<>();
+			private String typeName;
+
+			public P_InvocationHandler(Class<?> type) {
+				this.typeName = getTypeName(type);
 			}
-			return null;
-		}
 
-		private Object getValue(String propertyName, Class<?> returnType) {
-			
-			String resultValue = (new JSONObject(executeResult)).getString(propertyName);
-			// download png image given by id of result
-			Object result;
-			if (returnType.equals(Path.class)) {
-				Path p = Paths.get("/tmp/output/" + resultValue.split(":")[1] + ".png");
-				worker.downloadFile(resultValue, p.toString(), "png");
-				worker.deleteResource(resultValue);
-				result = p;
-			} else {
-				result = resultValue;
+			// TODO make better connection to real type - e.g. based on annotations
+			private String getTypeName(Class<?> type) {
+				return type.getPackage().getName() + type.getSimpleName().substring(1);
 			}
-			return result;
-		}
 
-		//TODO: make conversion more flexible 
-		private void setValue(String propertyName, Object object) {
-			if (object instanceof Path) {
-				Path path = (Path) object;
-				String ret = worker.uploadFile(path.toAbsolutePath().toString(), getFileType(path), path.getFileName().toString());
-				// obtain uploaded file id
-				object = new org.json.JSONObject(ret).getString("id");
+			@Override
+			public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+				if (method.getName().startsWith("set")) {
+					setValue(getPropertyName(method.getName()), args[0]);
+				} else if (method.getName().equals("run")) {
+					// execute worker
+					executeResult = worker.executeModule("command:" + typeName, this.args);
 
+				} else if (method.getName().startsWith("get")) {
+					return getValue(getPropertyName(method.getName()), method.getReturnType());
+				}
+				return null;
 			}
-			args.put(propertyName, object);
-		}
 
-		//TODO: support another types
-		private String getFileType(Path path) {
-			if(!path.endsWith(".png")) {
-				throw new UnsupportedOperationException("Only png files supported");
+			private Object getValue(String propertyName, Class<?> returnType) {
+
+				String resultValue = (new JSONObject(executeResult)).getString(propertyName);
+				// download png image given by id of result
+				Object result;
+				if (returnType.equals(Path.class)) {
+					Path p = Paths.get("/tmp/output/" + resultValue.split(":")[1] + ".png");
+					worker.downloadFile(resultValue, p.toString(), "png");
+					worker.deleteResource(resultValue);
+					result = p;
+				} else {
+					result = resultValue;
+				}
+				return result;
 			}
-			return "image/png";
+
+			// TODO: make conversion more flexible
+			private void setValue(String propertyName, Object object) {
+				if (object instanceof Path) {
+					Path path = (Path) object;
+					String ret = worker.uploadFile(path.toAbsolutePath().toString(), getFileType(path),
+							path.getFileName().toString());
+					// obtain uploaded file id
+					object = new org.json.JSONObject(ret).getString("id");
+
+				}
+				args.put(propertyName, object);
+			}
+
+			// TODO: support another types
+			private String getFileType(Path path) {
+				if (!path.endsWith(".png")) {
+					throw new UnsupportedOperationException("Only png files supported");
+				}
+				return "image/png";
+			}
+
+			private String getPropertyName(String name) {
+				String text = name.substring(3);
+				return text.substring(0, 1).toLowerCase() + text.substring(1);
+			}
+
 		}
 
-		private String getPropertyName(String name) {
-			String text = name.substring(3);
-			return text.substring(0,1).toLowerCase() + text.substring(1);
-		}
-		
 	}
 
 }
--- src/main/java/org/scijava/parallel/ParallelTask.java
+++ src/main/java/org/scijava/parallel/ParallelTask.java
@@ -1,6 +1,12 @@
 package org.scijava.parallel;
 
+import java.util.Map;
+
 public interface ParallelTask {
 
+	
 	<T> T getRemoteModule(Class<T> type);
+	
+	<T>void run(Class<T> type, Map<String, Object> inputMap);
+
 }
