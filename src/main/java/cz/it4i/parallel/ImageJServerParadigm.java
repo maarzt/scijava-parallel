c6a694b5c5c3f3b6960f9d05d3bf4fe5d95b825a
Petr Bainar <petr.bainar@hotmail.com>
2018-03-19 10:30:47 +0100

    parallelSupport: comment out persistence-related code

--- src/main/java/cz/it4i/parallel/ImageJServerParadigm.java
+++ src/main/java/cz/it4i/parallel/ImageJServerParadigm.java
@@ -3,7 +3,6 @@ package cz.it4i.parallel;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Collection;
-import java.util.LinkedHashMap;
 import java.util.LinkedList;
 import java.util.Map;
 
@@ -16,27 +15,28 @@ import org.slf4j.LoggerFactory;
 @Plugin(type = ParallelizationParadigm.class)
 public class ImageJServerParadigm extends SimpleOstravaParadigm {
 
+	// private static final String ADDRESS = "address";
 	private static final String PORT = "port";
-
-	private static final String ADDRESS = "address";
 
 	public static final Logger log = LoggerFactory.getLogger(cz.it4i.parallel.ImageJServerParadigm.class);
 
-	private Collection<String> hosts = new LinkedList<>();
+	private final Collection<String> hosts = new LinkedList<>();
 
 	@Override
 	public void init() {
 		if (poolSize == null) {
 			poolSize = Math.max(hosts.size(), 1);
 		}
-		retrieveConnectionConfig();
+		super.init();
+		
+		// Unused persistence code, to be revived later
+		/* retrieveConnectionConfig();
 		if (connectionConfig.size() == 0) {
 			Map<String, String> configEntries = new LinkedHashMap<>();
 			configEntries.put(ADDRESS, "localhost");
 			configEntries.put(PORT, "8080");
 			updateConnectionConfig(configEntries);
-		}
-		super.init();
+		} */
 	}
 
 	public void setHosts(Collection<String> hosts) {
