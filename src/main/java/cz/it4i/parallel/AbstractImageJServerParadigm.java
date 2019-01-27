0e37d882c3d6c07acca17d35ff251424eae6a882
Jan Kožusznik <jan@kozusznik.cz>
2019-01-27 20:05:53 +0100

    ssh launcher

---
+++ src/main/java/cz/it4i/parallel/AbstractImageJServerParadigm.java
@@ -0,0 +1,108 @@
+
+package cz.it4i.parallel;
+
+import static cz.it4i.parallel.Routines.supplyWithExceptionHandling;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.util.EntityUtils;
+import org.scijava.parallel.ParallelizationParadigm;
+import org.scijava.plugin.Plugin;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@Plugin(type = ParallelizationParadigm.class)
+public abstract class AbstractImageJServerParadigm extends
+	SimpleOstravaParadigm
+{
+
+	public static final Logger log = LoggerFactory.getLogger(
+		AbstractImageJServerParadigm.class);
+
+	private ParameterTypeProvider typeProvider = null;
+
+	@Override
+	protected void initWorkerPool() {
+		getHosts().forEach(host -> workerPool.addWorker(createWorker(host)));
+	}
+
+	private ParallelWorker createWorker(String host) {
+		final String[] tokensOfHost = host.split(":");
+		int port = Integer.parseInt(tokensOfHost[1]);
+		host = tokensOfHost[0];
+		synchronized (this) {
+			if (typeProvider == null) {
+				typeProvider = new P_ParameterTypeProvider(port, host);
+			}
+		}
+		return new ImageJServerWorker(host, port);
+	}
+
+	@Override
+	protected ParameterTypeProvider getTypeProvider() {
+		return typeProvider;
+	}
+
+	abstract protected Collection<String> getHosts();
+
+	private class P_ParameterTypeProvider implements ParameterTypeProvider {
+
+		private Map<String, Map<String, String>> mappedTypes = new HashMap<>();
+
+		private int port;
+		private String host;
+
+		public P_ParameterTypeProvider(int port, String host) {
+			super();
+			this.port = port;
+			this.host = host;
+		}
+
+		@Override
+		public String provideParameterTypeName(String commandName,
+			String parameterName)
+		{
+			Map<String, String> paramToClass = mappedTypes.computeIfAbsent(
+				commandName, c -> obtainCommandInfo(c));
+			return paramToClass.get(parameterName);
+		}
+
+		private Map<String, String> obtainCommandInfo(String commandTypeName) {
+			Map<String, String> result = new HashMap<>();
+			final String getUrl = "http://" + host + ":" + String.valueOf(port) +
+				"/modules/" + "command:" + commandTypeName;
+			final HttpGet httpGet = new HttpGet(getUrl);
+			final HttpResponse response = supplyWithExceptionHandling(
+				() -> HttpClientBuilder.create().build().execute(httpGet), log,
+				"get response");
+			org.json.JSONObject json = supplyWithExceptionHandling(
+				() -> new org.json.JSONObject(EntityUtils.toString(response
+					.getEntity())), log, "obtain command info");
+
+			org.json.JSONArray inputs = (org.json.JSONArray) json.get("inputs");
+			Iterator<?> iter = inputs.iterator();
+			while (iter.hasNext()) {
+				org.json.JSONObject param = (org.json.JSONObject) iter.next();
+				String typeName = ((String) param.get("genericType")).trim();
+				if (typeName.contains(" ")) {
+					typeName = typeName.split(" ")[1];
+				}
+				if (Character.isLowerCase(typeName.charAt(0)) && !typeName.contains(
+					"."))
+				{
+					typeName = "java.lang." + Character.toUpperCase(typeName.charAt(0)) +
+						typeName.substring(1);
+				}
+				result.put((String) param.get("name"), typeName);
+			}
+			return result;
+		}
+
+	}
+}
---
+++ src/main/java/cz/it4i/parallel/ClusterJobLauncher.java
@@ -0,0 +1,123 @@
+
+package cz.it4i.parallel;
+
+import com.jcraft.jsch.JSchException;
+
+import java.io.Closeable;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.joda.time.Instant;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import cz.it4i.fiji.scpclient.SshCommandClient;
+
+public class ClusterJobLauncher implements Closeable {
+
+	private final static Logger log = LoggerFactory.getLogger(
+		ClusterJobLauncher.class);
+
+	public class Job {
+
+		private String jobId;
+
+		public Job(String jobId) {
+			super();
+			this.jobId = jobId;
+		}
+
+		public void waitForRunning() {
+			if (jobId == null) {
+				throw new IllegalStateException("jobId not initialized");
+			}
+			String state;
+			String time;
+			do {
+				String result = client.executeCommand("qstat " + jobId).get(2);
+				String[] tokens = result.split(" +");
+				state = tokens[4];
+				time = tokens[3];
+				try {
+					Thread.sleep(1000);
+				}
+				catch (InterruptedException exc) {
+					log.error("waiting", exc);
+				}
+			}
+			while (!(!time.equals("0") && state.equals("R")));
+		}
+
+		public List<String> getNodes() {
+			if (jobId == null) {
+				throw new IllegalStateException("jobId not initialized");
+			}
+			List<String> result = client.executeCommand("qstat -f " + jobId);
+			List<String> hostLines = new LinkedList<>();
+			for (String line : result) {
+				if (hostLines.isEmpty() && line.contains("exec_host")) {
+					hostLines.add(line);
+				}
+				else if (!hostLines.isEmpty()) {
+					if (!line.contains("exec_vnode")) {
+						hostLines.add(line);
+					}
+					else {
+						break;
+					}
+				}
+			}
+			return new LinkedList<>(new HashSet<>(Arrays.asList(hostLines.stream()
+				.collect(Collectors.joining("")).replaceAll(" +", "").replaceAll(
+					"exec_host=", "").replaceAll("/[^+]+", "").split("\\+"))));
+		}
+
+	}
+
+	private SshCommandClient client;
+
+	public ClusterJobLauncher(String hostName, String userName,
+		String keyLocation, String keyPassword) throws JSchException
+	{
+		super();
+		this.client = new SshCommandClient(hostName, userName, keyLocation,
+			keyPassword);
+	}
+
+	public Job submit(String directory, String command, long usedNodes,
+		long ncpus)
+	{
+		String jobId = runJob(directory, command, usedNodes, ncpus);
+		return new Job(jobId);
+	}
+
+	public Job getSubmittedJob(String jobId) {
+		return new Job(jobId);
+	}
+
+	@Override
+	public void close() {
+		this.client.close();
+	}
+
+	private String runJob(String directory, String command, long nodes,
+		long ncpus)
+	{
+		String jobname = new Instant().getMillis() + "";
+		String fileName = jobname + ".sh";
+// @formatter:off
+		return client.executeCommand(
+			"echo \"" + 
+			"#!/usr/bin/env bash\n" +
+			"cd "+directory+"\n" +
+			"./"+command+"\n" +
+			"/usr/bin/tail -f /dev/null\" > " + fileName + " && " +
+			"chmod +x " + fileName +" &&" + 
+			"qsub  -q qexp -l select=" + nodes + ":ncpus=" + ncpus + " `readlink -f " + fileName + "`").get(0);
+// @formatter:on
+	}
+
+}
