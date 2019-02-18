b7e7075c4868a88633dc9d2082456fa8e8814eb5
Matthias Arzt <arzt@mpi-cbg.de>
2019-02-18 15:21:57 +0100

    Simplify two more examples

--- src/test/java/test/ScriptEvalRemotely.java
+++ src/test/java/test/ScriptEvalRemotely.java
@@ -6,6 +6,7 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 
+import cz.it4i.parallel.TestParadigm;
 import net.imagej.ImageJ;
 
 import org.scijava.command.Command;
@@ -15,35 +16,23 @@ import org.scijava.plugin.Plugin;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-@Plugin(type = Command.class, headless = true,
-	menuPath = "Plugins>DemonstrateOstravaParadigm")
-public class ScriptEvalRemotely extends AbstractBaseDemonstrationExample {
-
-	private static final Logger log = LoggerFactory.getLogger(AddDoubles.class);
-
-	@Parameter
-	private int step = 30;
+public class ScriptEvalRemotely {
 
 	public static void main(final String... args) {
 
 		final ImageJ ij = new ImageJ();
 		ij.ui().showUI();
-		ij.command().run(ScriptEvalRemotely.class, true);
-	}
-
-	@Override
-	protected void callRemotePlugin(ParallelizationParadigm paradigm) {
-		List<Map<String, Object>> paramsList = new LinkedList<>();
-		for (int i = 0; i < step; i++) {
-			Map<String, Object> params = new HashMap<>();
-			params.put("language", "ij1");
-			params.put("script", "print('hello from script" + i +
-				"'); getDirectory('home'); exec('whoami');");
-			paramsList.add(params);
+		try( ParallelizationParadigm paradigm = TestParadigm.localImageJServer( Config.getFijiExecutable(), ij.context() )) {
+			List<Map<String, Object>> paramsList = new LinkedList<>();
+			for (int i = 0; i < 10; i++) {
+				Map<String, Object> params = new HashMap<>();
+				params.put("language", "ij1");
+				params.put("script", "print('hello from script" + i +
+						"'); getDirectory('home'); exec('whoami');");
+				paramsList.add(params);
+			}
+			List<Map<String, Object>> result = paradigm.runAll(
+					"net.imagej.server.external.ScriptEval", paramsList);
 		}
-		List<Map<String, Object>> result = paradigm.runAll(
-			"net.imagej.server.external.ScriptEval", paramsList);
-		log.info("result: " + result);
-
 	}
 }
--- src/test/java/test/TestingCommandWithInterval.java
+++ src/test/java/test/TestingCommandWithInterval.java
@@ -1,50 +1,37 @@
 
 package test;
 
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 
+import cz.it4i.parallel.TestParadigm;
 import net.imagej.Extents;
 import net.imagej.ImageJ;
 
 import org.scijava.command.Command;
-import org.scijava.io.IOService;
 import org.scijava.parallel.ParallelizationParadigm;
-import org.scijava.plugin.Parameter;
 import org.scijava.plugin.Plugin;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 @Plugin(type = Command.class, headless = true)
-public class TestingCommandWithInterval extends
-	AbstractBaseDemonstrationExample
+public class TestingCommandWithInterval
 {
-
-	private final static Logger log = LoggerFactory.getLogger(
-		TestingCommandWithInterval.class);
-
 	public static void main(String[] args) {
 		final ImageJ ij = new ImageJ();
-		ij.command().run(TestingCommandWithInterval.class, true);
+		try( ParallelizationParadigm paradigm = TestParadigm.localImageJServer( Config.getFijiExecutable(), ij.context() )) {
+			Map<String, Object> parameters = new HashMap<>();
+			parameters.put("a", 11);
+			parameters.put("interval", new Extents(new long[] { 10 }, new long[] {
+				10 }));
+
+			List<Map<String, Object>> results = paradigm.runAll(
+					"net.imagej.server.TestingCommand", Collections.singletonList(parameters));
+			System.out.println("result: " + results);
+		}
 	}
 
-	@Override
-	protected void callRemotePlugin(ParallelizationParadigm paradigm) {
-		List<Map<String, Object>> parametersList = new LinkedList<>();
-		initParameters(parametersList);
-
-		List<Map<String, Object>> results = paradigm.runAll(
-			"net.imagej.server.TestingCommand", parametersList);
-		log.info("result: " + results);
-	}
-
-	private void initParameters(List<Map<String, Object>> parametersList) {
-		Map<String, Object> parameters = new HashMap<>();
-		parameters.put("a", 11);
-		parameters.put("interval", new Extents(new long[] { 10 }, new long[] {
-			10 }));
-		parametersList.add(parameters);
-	}
 }
